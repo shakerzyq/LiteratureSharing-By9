dc7bc6db2a3b420353313e7403acfb4d7b23a6d6
shakerzyq <1312197142@qq.com>
2020-06-06 00:29:24 +0800

    合并测试2

---
+++ app/src/main/java/com/example/literaturesharing/ZyqRegiste4Activity.java
@@ -0,0 +1,14 @@
+package com.example.literaturesharing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ZyqRegiste4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zyq_registe4);
+    }
+}
